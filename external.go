d316d5821c35b5b65ed9460b1c97188e3341a0e0
antimoth <2727577588@qq.com>
2018-09-19 18:02:45 +0800

    A: add method FromHex

--- external.go
+++ external.go
@@ -1,8 +1,27 @@
 package swaputils
 
 import (
+	"encoding/hex"
 	"github.com/ofgp/common/defines"
 )
+
+func Hex2Bytes(str string) []byte {
+	h, _ := hex.DecodeString(str)
+
+	return h
+}
+
+func FromHex(s string) []byte {
+	if len(s) > 1 {
+		if s[0:2] == "0x" || s[0:2] == "0X" {
+			s = s[2:]
+		}
+	}
+	if len(s)%2 == 1 {
+		s = "0" + s
+	}
+	return Hex2Bytes(s)
+}
 
 func CheckBytesToStr(checkBytes []byte, chainCode uint8) (string, error) {
 	switch chainCode {
