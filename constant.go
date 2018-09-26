182fb5e98852f87473c41c052f53a04145dd8b23
antimoth <2727577588@qq.com>
2018-09-26 14:06:11 +0800

    M: fix types

--- constant.go
+++ constant.go
@@ -2,11 +2,11 @@ package swaputils
 
 const (
 	BCH_ERROR_NET_ID         uint8 = 0xff
-	BCH_MAINNET_P2PKH_NET_ID       = 0xfe
-	BCH_MAINNET_P2SH_NET_ID        = 0xfd
-	BCH_TESTNET_P2PKH_NET_ID       = 0xfc
-	BCH_TESTNET_P2SH_NET_ID        = 0xfb
-	BCH_REGNET_P2PKH_NET_ID        = 0xfa
-	BCH_REGNET_P2SH_NET_ID         = 0xf9
-	ETH_NET_ID                     = 0xf8
+	BCH_MAINNET_P2PKH_NET_ID uint8 = 0xfe
+	BCH_MAINNET_P2SH_NET_ID  uint8 = 0xfd
+	BCH_TESTNET_P2PKH_NET_ID uint8 = 0xfc
+	BCH_TESTNET_P2SH_NET_ID  uint8 = 0xfb
+	BCH_REGNET_P2PKH_NET_ID  uint8 = 0xfa
+	BCH_REGNET_P2SH_NET_ID   uint8 = 0xf9
+	ETH_NET_ID               uint8 = 0xf8
 )
